1fd169c3c5159d78519f85284ef72684e71ca2fc
kylelingat <kylejlingat@gmail.com>
2018-08-29 08:51:10 -1000

    footer

---
+++ app.js
@@ -0,0 +1,22 @@
+$("#homeButton").click(function() {
+     $("html, body").animate({ scrollTop: 0 }, "slow");
+     return false;
+});
+
+$("#portButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".portfolio").offset().top
+    }, 1000);
+});
+
+$("#teamButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".meetTheTeam").offset().top
+    }, 1000);
+});
+
+$("#contactButton").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#footer").offset().top
+    }, 1000);
+});
